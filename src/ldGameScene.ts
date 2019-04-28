702ce09618cd89997cf655e529c1543ce7476de0
cheesepencil <cheesepencil@cheesepencil.com>
2019-04-27 20:52:51 -0400

    scrolling camera, physics

--- src/ldGameScene.ts
+++ src/ldGameScene.ts
@@ -4,6 +4,9 @@ const player: string = require('./images/player.png');
 const background: string = require('./images/background.png');
 
 export class LDGameScene extends Phaser.Scene {
+    private cursors: Phaser.Input.Keyboard.CursorKeys;
+    private playerSprite: Phaser.Physics.Arcade.Sprite;
+
     constructor() {
         super({ key: 'LDGameScene' })
     }
@@ -19,9 +22,34 @@ export class LDGameScene extends Phaser.Scene {
     }
 
     create(): void {
-        console.log('we arrived!');
-        this.add.image(100, 100, 'cat');
-        this.add.image(120, 160, 'background').setOrigin(0.5, 1);
+        this.cursors = this.input.keyboard.createCursorKeys();
+        let kittySprite = this.physics.add.sprite(100, 0, 'cat');
         this.cameras.main.fadeIn(1500, 0, 0, 0);
+        this.cameras.main.setBounds(-600, -160, 1200, 320);
+        let platforms = this.physics.add.staticGroup();
+        platforms.create(120, 150, 'background');
+        this.playerSprite = this.physics.add.sprite(50, 0, 'player');
+        this.cameras.main.startFollow(this.playerSprite, false, 0.1, 0.1);
+        this.playerSprite.setBounce(0.2);
+        this.physics.add.collider(this.playerSprite, platforms);
+        this.physics.add.collider(kittySprite, platforms);
+    }
+
+    update(): void {
+        let isTouchingDown = this.playerSprite.body.touching.down;
+
+        if (this.cursors.left.isDown && isTouchingDown) {
+            this.playerSprite.setVelocityX(-64);
+        }
+        else if (this.cursors.right.isDown && isTouchingDown) {
+            this.playerSprite.setVelocityX(64);
+        }
+        else if (isTouchingDown) {
+            this.playerSprite.setVelocityX(0);
+        }
+
+        if (this.cursors.space.isDown && isTouchingDown) {
+            this.playerSprite.setVelocityY(-96);
+        }
     }
 }
